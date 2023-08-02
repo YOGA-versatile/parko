5925c8a1eef89c675fe42c4ecff74beaf354e940
santhosh-vairamuthu <santhosh22032004@gmail.com>
2023-08-02 17:25:16 +0000

    init profile

--- frontend/host/templates/index.php
+++ frontend/host/templates/index.php
@@ -15,12 +15,11 @@ include "../libs/load.php";
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link href='https://fonts.googleapis.com/css?family=Heebo' rel='stylesheet'>
-    <script src="vendor/assets/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="../vendor/assets/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="../css/header.css">
     <link rel="stylesheet" href="../css/footer.css">
     <link rel="stylesheet" href="../css/index.css">
   </head>
-</head>
 
 <body>
   <!-- header -->
--- frontend/host/templates/profile.php
+++ frontend/host/templates/profile.php
@@ -1,28 +1,43 @@
-<<<<<<< HEAD
-
-=======
 <?php
-include "libs/load.php";
+include "../libs/load.php";
 ?>
-<head>
+
 <!doctype html>
 <html lang="en" data-bs-theme="auto">
-  <head>
-    <script src="vendor/assets/js/color-modes.js"></script>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.112.5">
-    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-    <link href="vendor/assets/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-    <script src="vendor/assets/dist/js/bootstrap.bundle.min.js"></script>
-  </head>
+
+<head>
+  <script src="../vendor/assets/js/color-modes.js"></script>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="generator" content="Hugo 0.112.5">
+  <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+  <link href="../vendor/assets/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+  <link href='https://fonts.googleapis.com/css?family=Heebo' rel='stylesheet'>
+  <script src="../vendor/assets/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="stylesheet" href="../css/header.css">
+  <link rel="stylesheet" href="../css/footer.css">
+  <link rel="stylesheet" href="../css/profile.css">
 </head>
 
 <body>
-    <?php load_temp("header.php"); ?>
+  <?php load_temp("header.php"); ?>
+  <main>
+    <div class="container jumbo-tron">
+      <img height="100px" src="../asset/pics/profile.png">
+      <h1 class="display-6 fw-bold">Hello Host</h1>
+      <div id="rating-container" class="align-items-center">
+        <span id="star-container"></span>
+        <span id="rating-count">(1307)</span>
+      </div>
+    </div>
+  </main>
+  <?php load_temp("footer.php"); ?>
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <script src="../js/profile.js"></script>
 </body>
->>>>>>> main
