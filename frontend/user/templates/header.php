b5418c75a189278564ad9ac659f8b802c58609b6
santhosh-vairamuthu <santhosh22032004@gmail.com>
2023-08-03 16:38:39 +0000

    new user template

---
+++ frontend/user/templates/header.php
@@ -0,0 +1,92 @@
+<header class="navbar navbar-expand-md custom-common-container ">
+        <div class="container-md custom-common-header">
+            <!-- Left-aligned item -->
+            <div class="navbar-brand">
+            <img class="icon justify-content-left" height="10px" src="../asset/pics/parko.png">
+            </div>
+            <!-- Center-aligned item -->
+            <ul class="navbar-nav mx-auto nav-pills">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php">Home</i></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="manage.php">Calender</i></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="listing.php">Listing</i></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="profile.php">Profile</i></a>
+              </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+            <li class="dropdown">
+              <p class="bi bi-list" data-toggle="dropdown"></p>
+              <ul class="dropdown-menu">
+                <li><a href="account.php"><i class="bi bi-person"></i> Account</a></li>
+                <li><a href="account.php"><i class="bi bi-person"></i> Profile</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a href="notification.php"><i class="bi bi-bell"></i> Notification</a></li>
+                <li><a href="help.php"><i class="bi bi-headset"></i> HelpCenter</a></li>
+                <li><a href="about.php"><i class="bi bi-info-circle"></i> About</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a href="#" class="tog">Switch - Guest</a></li>
+                <li><a href="#" class="tog"></i>Logout</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+</header>
+
+
+
+<!-- <div class="body-wrap">
+  <div class="container">
+    <nav class="navbar navbar-inverse" role="navigation">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Brand</a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Link</a></li>
+            <li><a href="#">Link</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li class="divider"></li>
+                <li><a href="#">Separated link</a></li>
+                <li class="divider"></li>
+                <li><a href="#">One more separated link</a></li>
+              </ul>
+            </li>
+          </ul>
+
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Link</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li class="divider"></li>
+                <li><a href="#">Separated link</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </div>
+</div> -->
