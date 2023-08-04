c44ffeb85684dbdbf0605b14e34df19c0c8ba51c
santhosh-vairamuthu <santhosh22032004@gmail.com>
2023-08-04 09:58:51 +0000

    new work

--- frontend/user/js/booking.js
+++ frontend/user/js/booking.js
@@ -1,115 +1,35 @@
-window.addEventListener("load", function () {
-const months = [
-    "January", "February", "March", "April", "May", "June", "July", "August",
-    "September", "October", "November", "December"
-];
+const stars = document.querySelectorAll('.star');
+const reviewRatingInput = document.getElementById('reviewRating');
 
-const prevBtn = document.getElementById("prev-btn");
-const nextBtn = document.getElementById("next-btn");
-const monthName = document.getElementById("month-name");
-const dateContainer = document.querySelector(".dates");
-const eventList = document.getElementById("event-list");
-const addEventForm = document.getElementById("add-event-form");
-const eventDescription = document.getElementById("event-description");
-const eventType = document.getElementById("event-type");
-
-let currentDate = new Date();
-let currentYear = currentDate.getFullYear();
-let currentMonth = currentDate.getMonth();
-let currentDay = currentDate.getDate();
-
-// Event listeners for prev and next buttons
-prevBtn.addEventListener("click", prev);
-nextBtn.addEventListener("click", next);
-
-// Initialize the calendar
-renderCalendar(currentYear, currentMonth);
-
-function prev() {
-    if (currentMonth > 0) {
-    currentMonth--;
-    } else {
-    currentYear--;
-    currentMonth = 11;
-    }
-    renderCalendar(currentYear, currentMonth);
-}
-
-function next() {
-    if (currentMonth < 11) {
-    currentMonth++;
-    } else {
-    currentYear++;
-    currentMonth = 0;
-    }
-    renderCalendar(currentYear, currentMonth);
-}
-
-function renderCalendar(year, month) {
-    monthName.innerText = months[month] + " " + year;
-    dateContainer.innerHTML = "";
-
-    const firstDayOfMonth = new Date(year, month, 1);
-    const lastDayOfMonth = new Date(year, month + 1, 0);
-    const numDays = lastDayOfMonth.getDate();
-
-    for (let i = 1; i <= numDays; i++) {
-        const dateItem = document.createElement("div");
-        dateItem.classList.add("date_item");
-        dateItem.textContent = i;
-        dateContainer.appendChild(dateItem);
-
-    if (i === currentDay && year === currentYear && month === currentMonth) {
-        dateItem.classList.add("present");
-    } else if (year < currentYear || (year === currentYear && month < currentMonth) ||
-                (year === currentYear && month === currentMonth && i < currentDay)) {
-        dateItem.classList.add("past");
-    } else {
-        dateItem.classList.add("future");
-    }
-
-    dateItem.addEventListener("click", function () {
-        currentDay = i;
-        updateEventList(year, month, i);
-        dateContainer.querySelectorAll(".date_item").forEach(item => item.classList.remove("present"));
-        dateItem.classList.add("present");
-    });
-    }
-
-    updateEventList(year, month, currentDay);
-}
-
-function updateEventList(year, month, day) {
-    eventList.innerHTML = "";
-    const events = getEvents(year, month, day);
-    events.forEach(event => {
-    const li = document.createElement("li");
-    li.textContent = `It's a ${event.type} thing - ${event.description}`;
-    eventList.appendChild(li);
-    });
-}
-
-function getEvents(year, month, day) {
-    const id = day.toString() + month.toString();
-    return events.filter(event => event.id === id);
-}
-
-let events = [];
-
-addEventForm.addEventListener("submit", function (event) {
-    event.preventDefault();
-    const description = eventDescription.value.trim();
-    if (description === "") return;
-
-    const id = currentDay.toString() + currentMonth.toString();
-    const type = eventType.value;
-    events.push({
-    id: id,
-    description: description,
-    type: type
-    });
-
-    updateEventList(currentYear, currentMonth, currentDay);
-    eventDescription.value = "";
+stars.forEach(star => {
+  star.addEventListener('click', () => {
+    const rating = parseInt(star.getAttribute('data-rating'));
+    setRating(rating);
+  });
 });
-});
+
+function setRating(rating) {
+  stars.forEach((star, index) => {
+    if (index < rating) {
+      star.classList.add('gold-star'); // Apply gold color
+    } else {
+      star.classList.remove('gold-star'); // Remove gold color
+    }
+  });
+  reviewRatingInput.value = rating;
+}
+
+function submitReview() {
+  const rating = parseInt(reviewRatingInput.value);
+  const reviewText = document.querySelector('textarea').value;
+  // Perform your review submission logic here, e.g., AJAX call to submit the review with the selected rating and reviewText.
+  console.log("Rating: " + rating);
+  console.log("Review Text: " + reviewText);
+}
+
+
+function calculateValue() {
+    const userInput = document.getElementById('userInput').value;
+    const calculatedValue = userInput * 5; // Change this multiplier as needed
+    document.getElementById('calculatedValue').value = "₹ "+calculatedValue;
+  }
--- frontend/user/templates/booking.php
+++ frontend/user/templates/booking.php
@@ -18,14 +18,107 @@ include "../libs/load.php";
     <script src="../vendor/assets/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="../css/header.css">
     <link rel="stylesheet" href="../css/footer.css">
-    <link rel="stylesheet" href="../css/index.css">
+    <link rel="stylesheet" href="../css/booking.css">
   </head>
 
 <body>
   <!-- header -->
     <?php load_temp("header.php"); ?>
+    <div class="container mt-3 booking-container">
+
+    <div class="card mb-2 mt-1 book-card">
+      <div class="card-header">
+        TN 96 E 3448 <span class="badge text-bg-success">parked</span>
+      </div>
+      <div class="card-body">
+        <p class="card-text">Booking ID : #176-590</p>
+        <p class="card-text">Vehicle Type : Motor Two Wheeler</p>
+        <p class="card-text">Booked Date : 04/08/2023</p>
+        <p class="card-text">Parking Time : 2.00PM - 5.30 PM [1.30 Hours Remaining]</p>
+        <a href="#" class="btn btn-primary" type="button" data-bs-toggle="modal" data-bs-target="#extendModal">Extend Time</a>
+      </div>
+    </div>
+
+    <div class="card mb-2 mt-1 book-card">
+      <div class="card-header">
+        TN 96 E 3448 <span class="badge text-bg-info">yet to go</span>
+      </div>
+      <div class="card-body">
+        <p class="card-text">Booking ID : #176-590</p>
+        <p class="card-text">Vehicle Type : Motor Two Wheeler</p>
+        <p class="card-text">Booked Date : 04/08/2023</p>
+        <p class="card-text">Parking Time : 2.00PM - 5.30 PM [3.30 Hours]</p>
+        <a href="map.php?lat=9.9315573&lon=78.1022729" class="btn btn-primary">Directions</a>
+      </div>
+    </div>
+
+    <div class="card mb-2 mt-1 book-card">
+      <div class="card-header">
+        TN 96 E 3448 <span class="badge text-bg-secondary">completed</span>
+      </div>
+      <div class="card-body">
+        <p class="card-text">Booking ID : #176-590</p>
+        <p class="card-text">Vehicle Type : Motor Two Wheeler</p>
+        <p class="card-text">Booked Date : 04/08/2023</p>
+        <p class="card-text">Parking Time : 2.00PM - 5.30 PM </p>
+        <a href="#" class="btn btn-primary" type="button" data-bs-toggle="modal" data-bs-target="#reviewModal">Review</a>
+      </div>
+    </div>
+
+    <div class="modal fade" id="extendModal" tabindex="-1" aria-labelledby="extendModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="extendModalLabel">Extend</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    
+                <form class="mb-3">
+                    <label for="userInput" class="form-label">Enter Time to be Extend (minimum 1 hour):</label>
+                    <input type="number" class="form-control" id="userInput" min="1" onchange="calculateValue()">
+                    <label for="calculatedValue" class="form-label">Price :</label>
+                    <input type="text" class="form-control" id="calculatedValue" readonly>
+                    <button type="submit" class="btn btn-info mt-3">Pay & Proceed</button>
+                </form>
+              </div>
+              <div class="modal-footer">
+              </div>
+              </div>
+            </div>
+        </div>
+
+
+    <div class="modal fade" id="reviewModal" tabindex="-1" aria-labelledby="reviewModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="reviewModalLabel">Give Your Review</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  <div class="star-rating" data-rating="0">
+                    <span class="star" data-rating="1">&#9733;</span>
+                    <span class="star" data-rating="2">&#9733;</span>
+                    <span class="star" data-rating="3">&#9733;</span>
+                    <span class="star" data-rating="4">&#9733;</span>
+                    <span class="star" data-rating="5">&#9733;</span>
+                  </div>
+                  <input type="hidden" name="reviewRating" id="reviewRating" value="0">
+                  <textarea class="form-control mt-2" rows="4" placeholder="Write your review here"></textarea>
+                  <button type="button" class="btn btn-primary mt-2" onclick="submitReview()">Submit Review</button>
+                </div>
+                <div class="modal-footer">
+                </div>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
     <?php load_temp("footer.php"); ?>
     <!-- Add the Bootstrap JS and jQuery scripts -->
+    <script src="../js/booking.js"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
