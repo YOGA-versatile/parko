d95c2fdff14ede94c52f336d4a48aa88765b4a76
santhosh-vairamuthu <santhosh22032004@gmail.com>
2023-08-05 08:25:04 +0000

    header completed

--- frontend/host/templates/header.php
+++ frontend/host/templates/header.php
@@ -1,39 +1,38 @@
-<header class="navbar navbar-expand-md custom-common-container ">
-        <div class="container-md custom-common-header">
-            <!-- Left-aligned item -->
-            <div class="navbar-brand">
-            <img class="icon justify-content-left" height="10px" src="/frontend/host/asset/pics/parko.png">
-            </div>
-            <!-- Center-aligned item -->
-            <ul class="navbar-nav mx-auto nav-pills">
-              <li class="nav-item">
-                <a class="nav-link" href="index.php">Home</i></a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="manage.php">Calender</i></a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="listing.php">Listing</i></a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="profile.php">Profile</i></a>
-              </li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-            <li class="dropdown">
-              <p class="bi bi-list" data-toggle="dropdown"></p>
-              <ul class="dropdown-menu">
-                <li><a href="account.php"><i class="bi bi-person"></i> Account</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a href="notification.php"><i class="bi bi-bell"></i> Notification</a></li>
-                <li><a href="help.php"><i class="bi bi-headset"></i> HelpCenter</a></li>
-                <li><a href="about.php"><i class="bi bi-info-circle"></i> About</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a href="../../user/templates" class="tog">Switch - Guest</a></li>
-                <li><a href="../../auth.php" class="tog"></i>Logout</a></li>
-              </ul>
-            </li>
-          </ul>
+<header class="navbar navbar-expand-md custom-common-container">
+    <div class="container-md custom-common-header">
+        <!-- Left-aligned item -->
+        <div class="navbar-brand">
+            <img class="icon justify-content-left" height="40px" src="../asset/pics/parko.png" alt="Parko Logo">
         </div>
+        <!-- Center-aligned item -->
+        <ul class="navbar-nav mx-auto nav-pills">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="manage.php">Calendar</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="listing.php">Listing</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="profile.php">Profile</a>
+            </li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+            <li class="dropdown dropdown-end">
+                <a href="#" class="bi bi-list" data-bs-toggle="dropdown"></a>
+                <ul class="dropdown-menu dropdown-menu-end">
+                    <li><a href="account.php"><i class="bi bi-person"></i> Account</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a href="notification.php"><i class="bi bi-bell"></i> Notification</a></li>
+                    <li><a href="help.php"><i class="bi bi-headset"></i> Help Center</a></li>
+                    <li><a href="about.php"><i class="bi bi-info-circle"></i> About</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a href="../../user/templates" class="tog">Switch - Guest</a></li>
+                    <li><a href="../../auth.php" class="tog">Logout</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
 </header>
-
